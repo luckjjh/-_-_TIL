fefb276ced90a066e9ff7d33bf782ee71a6d6236
luckjjh <luckjjh1@naver.com>
2022-04-27 22:08:46 +0900

    0427 수업자료 commit

---
+++ 0427/ll_0427/JavaScript/002.js
@@ -0,0 +1,5 @@
+document.getElementById("one").innerHTML =
+  "hello <strong>world</strong> document";
+console.log("hello world console");
+window.alert("hello world alert");
+document.write("hello!!");
